e0cf05e2970651bcb73edc81d9ed06d6ced8d59d
Maarten <jachtschipwong@hotmail.com>
2018-07-13 12:10:05 +0200

    Attempt to create a platform class
    I tried to create a platform, but failed.

--- player.py
+++ player.py
@@ -9,7 +9,6 @@ class player():
     player_width = 40
     player_height = 40
     player_color = (255, 215, 0)
-
     # Player Constructor
     def __init__(self, player_width, player_height, player_color):
         self.player_width = player_width
@@ -18,14 +17,13 @@ class player():
         self.player_color = player_color
         self.player_vel = 0
         self.player_acc = 0
-        self.center = self.player_pos
 
     def update_player(self):
-        #Background color
-        gameDisplay.fill(black)
         #Draw player
         pygame.draw.rect(gameDisplay, self.player_color,
-                         (self.player_pos.x, self.player_pos.y, self.player_width, self.player_height))
+                         (self.player_pos.x, self.player_pos.y, self.player_width,
+                          self.player_height))
+
         # Border collision
         if (self.player_pos.x > (screen_width - self.player_width)):
             self.player_pos.x = screen_width - self.player_width
@@ -42,10 +40,10 @@ class player():
             self.player_vel = 0
 
         # Font and text rendering
-        actual_x = myFont.render(str(self.player_pos.x), False, (white))
-        actual_y = myFont.render(str(self.player_pos.y), False, (white))
-        actual_a = myFont.render(str(self.player_acc), False, (white))
-        actual_v = myFont.render(str(self.player_vel), False, (white))
+        actual_x = myFont.render(str(int(self.player_pos.x)), False, (white))
+        actual_y = myFont.render(str(int(self.player_pos.y)), False, (white))
+        actual_a = myFont.render(str(float(self.player_acc)), False, (white))
+        actual_v = myFont.render(str(float(self.player_vel)), False, (white))
 
         #More rendering
         gameDisplay.blit(xcolon, (0, 0))
@@ -57,7 +55,23 @@ class player():
         gameDisplay.blit(vcolon, (0,90))
         gameDisplay.blit(actual_v, (25, 90))
 
-        #Fps
-        clock.tick(FPS)
         #Updat screen
         pygame.display.update()
+
+class platform():
+    platform_color = yellow
+    #Constructor
+    def __init__(self, x, y, color, width, height):
+        self.platform_width = width
+        self.platform_height = height
+        self.platform_color = color
+        self.plat_pos = vec(0, 0)
+        self.plat_pos.x = x
+        self.plat_pos.y = y
+
+    def platform_update(self):
+        pygame.draw.rect(gameDisplay, self.platform_color,
+                         (self.plat_pos.x, self.plat_pos.y, self.platform_width,
+                          self.platform_height))
+        pygame.display.update()
+
