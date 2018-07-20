eaf61458c540acb4f638cd6b3237badc02203529
Maarten <jachtschipwong@hotmail.com>
2018-07-21 01:30:46 +0200

    Some changes i dunno lol

--- settings.py
+++ settings.py
@@ -1,10 +1,11 @@
 screen_width = 800
 screen_height = 600
 
-yellow = (255, 215, 0)
+yellow = (193, 115, 81)
 black = (0, 0, 0)
+background_color = (45, 20, 91)
 white = (255, 255, 255)
-red = (198, 55, 84)
+ground = (122, 142, 69)
 FPS = 60
 
 PLAYER_ACC = .3
